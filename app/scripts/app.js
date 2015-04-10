757723a675fc4673bbc0e872a2ee6b84d94cdf27
Anh Trinh <anh@catalyst.net.nz>
2015-04-10 17:48:59 +1200

    yeoman scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name soundcloudifyApp
+ * @description
+ * # soundcloudifyApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('soundcloudifyApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soundcloudifyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the soundcloudifyApp
+ */
+angular.module('soundcloudifyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
