6411a000a476e8cfd95dd473584848a67727a52c
Anh Trinh <anh.trinhtrung@gmail.com>
2015-04-12 00:53:54 +1200

    fix broken artwork

--- Gruntfile.js
+++ Gruntfile.js
@@ -266,7 +266,7 @@ module.exports = function (grunt) {
 
     // Performs rewrites based on filerev and the useminPrepare configuration
     usemin: {
-      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      html: ['<%= yeoman.dist %>/{,*/}*.html', '<%= yeoman.dist %>/scripts/views/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
       options: {
         assetsDirs: [
